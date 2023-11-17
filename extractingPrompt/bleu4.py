6c4c2121a2c96df10e072a0fc542fe380baddaf8
liangzid <2273067585@qq.com>
2023-11-17 21:51:46 +0800

    add some newthings to metics.

--- extractingPrompt/bleu4.py
+++ extractingPrompt/bleu4.py
@@ -18,12 +18,11 @@ from __future__ import unicode_literals
 
 # ------------------------ Code --------------------------------------
 
-## normal import 
+# normal import
 import json
-from typing import List,Tuple,Dict
+from typing import List, Tuple, Dict
 import random
 from pprint import pprint as ppp
-
 
 
 import math
@@ -31,6 +30,8 @@ import copy
 
 # From https://github.com/zhyack/BLEU4Python/blob/master/bleu.py
 # All rights belong to the original author
+
+
 def bleu_count(hypothesis, references, max_n=4):
     ret_len_hyp = 0
     ret_len_ref = 0
@@ -53,21 +54,21 @@ def bleu_count(hypothesis, references, max_n=4):
             if diff < closest_diff:
                 closest_diff = diff
                 closest_length = y_len[i]
-            elif diff==closest_diff and y_len[i] < closest_length:
+            elif diff == closest_diff and y_len[i] < closest_length:
                 closest_length = y_len[i]
 
             for n in range(max_n):
                 sent_ngram = dict()
                 for st in range(0, y_len[i]-n):
-                    ngram = "%d"%(n+1)
+                    ngram = "%d" % (n+1)
                     for k in range(n+1):
                         j = st+k
-                        ngram += " %s"%(y[i][j])
+                        ngram += " %s" % (y[i][j])
                     if ngram not in sent_ngram:
-                        sent_ngram[ngram]=0
-                    sent_ngram[ngram]+=1
+                        sent_ngram[ngram] = 0
+                    sent_ngram[ngram] += 1
                 for ngram in sent_ngram.keys():
-                    if ngram not in ref_ngram or ref_ngram[ngram]<sent_ngram[ngram]:
+                    if ngram not in ref_ngram or ref_ngram[ngram] < sent_ngram[ngram]:
                         ref_ngram[ngram] = sent_ngram[ngram]
 
         ret_len_hyp += x_len
@@ -76,38 +77,43 @@ def bleu_count(hypothesis, references, max_n=4):
         for n in range(max_n):
             hyp_ngram = dict()
             for st in range(0, x_len-n):
-                ngram = "%d"%(n+1)
+                ngram = "%d" % (n+1)
                 for k in range(n+1):
                     j = st+k
-                    ngram += " %s"%(x[j])
+                    ngram += " %s" % (x[j])
                 if ngram not in hyp_ngram:
-                    hyp_ngram[ngram]=0
-                hyp_ngram[ngram]+=1
+                    hyp_ngram[ngram] = 0
+                hyp_ngram[ngram] += 1
             for ngram in hyp_ngram.keys():
                 if ngram in ref_ngram:
-                    ret_clip_count[n] += min(ref_ngram[ngram], hyp_ngram[ngram])
+                    ret_clip_count[n] += min(ref_ngram[ngram],
+                                             hyp_ngram[ngram])
                 ret_count[n] += hyp_ngram[ngram]
 
     return ret_clip_count, ret_count, ret_len_hyp, ret_len_ref
 
 # From https://github.com/zhyack/BLEU4Python/blob/master/bleu.py
 # All rights belong to the original author
+
+
 def corpus_bleu(hypothesis, references, max_n=4):
-    assert(len(hypothesis) == len(references))
-    clip_count, count, total_len_hyp, total_len_ref = bleu_count(hypothesis, references, max_n=max_n)
+    assert (len(hypothesis) == len(references))
+    clip_count, count, total_len_hyp, total_len_ref = bleu_count(
+        hypothesis, references, max_n=max_n)
     brevity_penalty = 1.0
     bleu_scores = []
     bleu = 0
     for n in range(max_n):
-        if count[n]>0:
+        if count[n] > 0:
             bleu_scores.append(clip_count[n]/count[n])
         else:
             bleu_scores.append(0)
     if total_len_hyp < total_len_ref:
-        if total_len_hyp==0:
+        if total_len_hyp == 0:
             brevity_penalty = 0.0
         else:
             brevity_penalty = math.exp(1 - total_len_ref/total_len_hyp)
+
     def my_log(x):
         if x == 0:
             return -9999999999.0
@@ -121,11 +127,6 @@ def corpus_bleu(hypothesis, references, max_n=4):
     return [bleu]+bleu_scores, [brevity_penalty, total_len_hyp/total_len_ref, total_len_hyp, total_len_ref]
 
 
-
-
-
-## running entry
-if __name__=="__main__":
+# running entry
+if __name__ == "__main__":
     print("EVERYTHING DONE.")
-
-
---
+++ extractingPrompt/metrics_with_LMs.py
@@ -0,0 +1,66 @@
+"""
+======================================================================
+METRICS_WITH_LMS ---
+
+High-level evluation, about the quality of LMs.
+
+    Author: Zi Liang <frost.liang@polyu.edu.hk>
+    Copyright © 2023, ZiLiang, all rights reserved.
+    Created: 17 November 2023
+======================================================================
+"""
+
+
+# ------------------------ Code --------------------------------------
+
+## normal import 
+import json
+from typing import List,Tuple,Dict
+import random
+from pprint import pprint as ppp
+# import pickle
+# import os
+# from os.path import join, exists
+# from collections import Counter,OrderedDict
+# from bisect import bisect
+# from copy import deepcopy
+# import pickle
+
+# import sys
+# # sys.path.append("./")
+# from tqdm import tqdm
+
+# import numpy as np
+
+# import argparse
+# import logging
+
+
+## deep learning related import
+# import numpy as np
+
+# import torch
+# from torch.utils.tensorboard import SummaryWriter
+# from torch.nn import DataParallel
+# from torch.utils.data import Dataset, DataLoader
+# from torch.nn import CrossEntropyLoss
+# import torch.nn as nn
+# from torch.nn.parallel import DistributedDataParallel
+# from torchvision.transforms import Compose as ComposeTransformation
+# import tensorboardX
+
+# ## transformers related import
+# from transformers import T5Tokenizer,T5ForConditionalGeneration
+# from transformers import BertTokenizer
+# from transformers import pipeline
+# import transformers
+
+
+
+
+## running entry
+if __name__=="__main__":
+    main()
+    print("EVERYTHING DONE.")
+
+
